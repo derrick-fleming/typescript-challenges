9af0a2b1cf68cd78e31d83a4d5ff8adc7cc85638
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-14 20:08:14 +0000

    Updated typing tutor to include a randomizer between two different texts, and a calculation of accuracy. Also, used 'forEach' instead of 'map'.

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,25 +1,40 @@
-/* eslint-disable no-unused-vars */
 var $text = document.querySelector('.text');
-var phrase = 'grumpy wizards make toxic brew';
+var $score = document.querySelector('.hidden');
+var randomNumber = Math.random() * 100;
+var phrase = randomNumber >= 50
+  ? 'grumpy wizards make toxic brew'
+  : 'What a beautiful day it is on the beach, here in beautiful and sunny Hawaii.';
 var phraseArray = phrase.split('');
 var spanIndex = 0;
-var typingChallenge = phraseArray.map(function (phrase, index) {
+var accuracyScore = 0;
+phraseArray.forEach(function (phrase) {
   var $span = document.createElement('span');
   $span.textContent = phrase;
   if (phrase === ' ') {
     $span.className = 'white-space';
   }
-  return $text.appendChild($span);
+  $text.appendChild($span);
 });
 var $span = document.querySelectorAll('span');
 document.addEventListener('keydown', handleTyping);
 $span[0].className = 'border';
 function handleTyping(e) {
+  if (spanIndex >= $span.length) {
+    return;
+  }
   if (e.key !== $span[spanIndex].textContent) {
     $span[spanIndex].className = 'text-wrong';
+    accuracyScore--;
     return;
   }
   $span[spanIndex].className = 'text-correct';
   spanIndex++;
+  accuracyScore++;
+  if (spanIndex >= $span.length) {
+    var calculateScore = Math.floor(100 * (accuracyScore / ($span.length)));
+    $score.className = 'score';
+    $score.textContent = 'Your Accuracy: '.concat(calculateScore, '%');
+    return;
+  }
   $span[spanIndex].className = 'border';
 }
--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -1,29 +1,51 @@
 const $text = document.querySelector('.text') as HTMLHeadingElement;
+const $score = document.querySelector('.hidden') as HTMLHeadingElement;
 
-const phrase = 'grumpy wizards make toxic brew';
+const randomNumber = Math.random() * 100;
+const phrase = randomNumber >= 50
+  ? 'grumpy wizards make toxic brew'
+  : 'What a beautiful day it is on the beach, here in beautiful and sunny Hawaii.';
+
 const phraseArray = phrase.split('');
 let spanIndex = 0;
+let accuracyScore = 0;
 
-const typingChallenge = phraseArray.map((phrase, index) => {
+phraseArray.forEach((phrase) => {
   const $span = document.createElement('span');
   $span.textContent = phrase
   if (phrase === ' ') {
     $span.className = 'white-space'
   }
-  return $text.appendChild($span);
+  $text.appendChild($span);
 })
 
 const $span = document.querySelectorAll('span') as NodeListOf<HTMLSpanElement>;
 document.addEventListener('keydown', handleTyping);
 
+
 $span[0].className = 'border';
 
 function handleTyping(e: KeyboardEvent) {
+  if (spanIndex >= $span.length) {
+    return;
+  }
+
   if (e.key !== $span[spanIndex].textContent) {
     $span[spanIndex].className = 'text-wrong';
+    accuracyScore--;
     return;
   }
   $span[spanIndex].className = 'text-correct';
+
   spanIndex++
+  accuracyScore++
+
+  if (spanIndex >= $span.length) {
+    const calculateScore = Math.floor(100 * (accuracyScore / ($span.length)))
+    $score.className='score';
+    $score.textContent = `Your Accuracy: ${calculateScore}%`
+    return;
+  }
+
   $span[spanIndex].className = 'border'
 }
