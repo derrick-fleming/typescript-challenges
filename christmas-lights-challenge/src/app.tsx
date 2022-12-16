ee809faa45ea892e1d22bb89123022d6196bc808
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-16 22:11:16 +0000

    Added two additional lights to the DOM and updated styling of lights to be different times/pace.

--- christmas-lights-challenge/src/app.tsx
+++ christmas-lights-challenge/src/app.tsx
@@ -4,37 +4,51 @@ export default function App() {
   return (
   <div className='container'>
     <div className='row'>
-      <div className='column-one-fifth padding-x3'>
+      <div className='column-one-seventh padding-x3'>
         <div className='row'>
             <svg className='red' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
               <circle cx="50" cy="50" r="50" />
           </svg>
         </div>
       </div>
-        <div className='column-one-fifth padding-x3'>
+        <div className='column-one-seventh padding-x3'>
           <div className='row'>
             <svg className='yellow' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
               <circle cx="50" cy="50" r="50" />
             </svg>
           </div>
       </div>
-        <div className='column-one-fifth padding-x3'>
+        <div className='column-one-seventh padding-x3'>
           <div className='row'>
             <svg className='orange' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
               <circle cx="50" cy="50" r="50" />
             </svg>
           </div>
       </div>
-        <div className='column-one-fifth padding-x3'>
+        <div className='column-one-seventh padding-x3'>
         <div className='row'>
           <svg className='green' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <circle cx="50" cy="50" r="50" />
           </svg>
         </div>
       </div>
-      <div className='column-one-fifth padding-x3'>
+      <div className='column-one-seventh padding-x3'>
         <div className='row'>
-          <svg className='red' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <svg className='red-two' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="50" />
+          </svg>
+        </div>
+      </div>
+      <div className='column-one-seventh padding-x3'>
+        <div className='row'>
+          <svg className='orange' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="50" />
+          </svg>
+        </div>
+      </div>
+      <div className='column-one-seventh padding-x3'>
+        <div className='row'>
+          <svg className='green' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <circle cx="50" cy="50" r="50" />
           </svg>
         </div>
