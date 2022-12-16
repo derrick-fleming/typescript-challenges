b9436803a2518067d32c72f40bf60d00c348e474
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-16 21:50:07 +0000

    Created five circles to simulate christmas lights and styled them to be red, yellow, orange, and green.

--- christmas-lights-challenge/src/app.tsx
+++ christmas-lights-challenge/src/app.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
 
 export default function App() {
-  return <div><h1>Hello</h1></div>
+  return (
+  <div className='container'>
+    <div className='row'>
+      <div className='column-one-fifth padding-x3'>
+        <div className='row'>
+            <svg className='red' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="50" cy="50" r="50" />
+          </svg>
+        </div>
+      </div>
+        <div className='column-one-fifth padding-x3'>
+          <div className='row'>
+            <svg className='yellow' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="50" cy="50" r="50" />
+            </svg>
+          </div>
+      </div>
+        <div className='column-one-fifth padding-x3'>
+          <div className='row'>
+            <svg className='orange' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="50" cy="50" r="50" />
+            </svg>
+          </div>
+      </div>
+        <div className='column-one-fifth padding-x3'>
+        <div className='row'>
+          <svg className='green' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="50" />
+          </svg>
+        </div>
+      </div>
+      <div className='column-one-fifth padding-x3'>
+        <div className='row'>
+          <svg className='red' viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="50" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>)
 }
