14588047d3cfbf03359ff9384977c2c14bd1fb26
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-17 02:21:12 +0000

    Updated create-display to create event handlers that changed color of lightbulb and started/stopped animation when user clicked 'start' and 'stop'.

--- christmas-lights-challenge/src/create-display.tsx
+++ christmas-lights-challenge/src/create-display.tsx
@@ -1,20 +1,41 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const colors = ['yellow', 'red', 'green', 'orange'];
 const seven = [0, 1, 2, 3, 4, 5, 6, 7]
 
 export default function CreateDisplay() {
-  const lights = seven.map(number => <Lights key={number}/>)
+  const [random, setRandom] = useState(0);
+  const [timerOn, setTimerOn] = useState(false)
+
+  const lights = seven.map(number => <Lights timerOn={timerOn} random={random} key={number}/>)
+
+  function handleStart(){
+    if (!timerOn) {
+      setTimerOn(true);
+      const randomNumber = Math.floor(Math.random() * 10);
+      setRandom(randomNumber);
+    } else {
+      setTimerOn(false);
+    }
+  }
+
+  const text = timerOn ? 'Stop' : 'Start'
+
   return (
     <div className='container'>
       <div className='row'>
         {lights}
       </div>
+      <div className="row container space-between">
+        <button onClick={handleStart} className="button">
+          {text}
+        </button>
+      </div>
     </div>
   )
 }
 
-function Lights() {
+function Lights(props: {key: number, random: number, timerOn: boolean}) {
   const [color, setColor] = useState('yellow');
 
   function handleClick() {
@@ -25,11 +46,16 @@ function Lights() {
       }
     })
   }
+  let animation = props.timerOn ? {animation: `${color}-color ${props.random}s ease-in-out infinite`} : {}
+  if (color === 'red' && props.timerOn) {
+    animation = { animation: `${color}-color-1 ${props.random}s ease-in-out infinite` }
+  }
+
 
   return (
     <div className='column-one-seventh padding-x3'>
       <div className='row'>
-        <svg onClick={handleClick} className={color} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <svg style={animation} onClick={handleClick} className={color} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
           <circle cx="50" cy="50" r="50" />
         </svg>
       </div>
