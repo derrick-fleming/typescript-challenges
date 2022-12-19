9a12278b958117a124eecafa33136b583170b38d
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-19 22:07:22 +0000

    Adjusted styling of text to move underline when moving to next letter. Beginning to create score functionality with test.

--- react-typescript-tutor/src/app.tsx
+++ react-typescript-tutor/src/app.tsx
@@ -2,17 +2,19 @@ import React, {useState, useEffect} from "react";
 import useKeyPress from "./keypress-hook";
 
 const App = () => {
-  const [text, setText] = useState(null);
+  const [text, setText]: [string|null, React.Dispatch<React.SetStateAction<string>>] = useState(null);
   const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
   const phrase = 'grumpy wizards make toxic brew'
   const phraseArray = phrase.split('');
 
   useEffect(() => {
     if (text === phrase[index]) {
       setIndex(index + 1)
-      setText(null);
+      setText(null)
     }
-  })
+  }, [text, index])
+
 
   const phraseMapped = phraseArray.map((letter, i) => {
     let textClass = '';
@@ -31,17 +33,20 @@ const App = () => {
     return <span className={textClass} key={i}>{letter}</span>;
   })
 
+  const scoreClass = index === phrase.length ? 'score' : 'hidden'
+
   useKeyPress(key => {
     setText(key);
   });
-
-  console.log(phraseMapped);
 
   return(
     <div className="container">
       <h1 className="text">
         {phraseMapped}
       </h1>
+      <h2 className={scoreClass}>
+        {score}
+      </h2>
     </div>
   )
 }
