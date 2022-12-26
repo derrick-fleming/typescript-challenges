f829091d7d9ee14733e74582189d323ec5e1d2d7
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-26 22:53:26 +0000

    Created a new folder with basic typescript and react functionality to build a stopwatch component.

---
+++ react-typescript-stopwatch/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+const container = document.querySelector('#root') as HTMLDivElement;
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
