91f86dcdb728c56a04ca274fcc5865eb44076169
Derrick Fleming <derrick.fleming7@gmail.com>
2022-12-26 23:19:00 +0000

    Created timer using typescript and react hooks with specified types for the interval. Styled border of stopwatch to change colors on an animation cycle when timer is going.

--- react-typescript-stopwatch/src/app.tsx
+++ react-typescript-stopwatch/src/app.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-const App = function(){
-  return <h1>Hello World</h1>
-}
+const App = (): JSX.Element => {
+  const [count, setCount] = useState(0);
+  const [play, setPlay] = useState(false);
+
+  function toggleStart() {
+    if (play !== false) {
+      setPlay(false);
+    } else {
+      setPlay(true);
+    }
+  }
+
+  useEffect(() => {
+
+    let interval: ReturnType<typeof setInterval> | null = null;
+    if (play) {
+      interval = setInterval(() => setCount(prevCount => prevCount + 1), 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [play]);
+
+  function resetTimer(): void {
+    if (!play) {
+      setCount(0);
+    }
+  }
+
+  const iconClass = play
+    ? 'pause'
+    : 'play';
+
+  const border = iconClass === 'play'
+    ? 'watch-face'
+    : 'change-color'
+
+  return (
+    <div className='container'>
+      <div className={border} onClick={resetTimer}>
+        <h1>{count}</h1>
+      </div>
+      <div className='container'>
+        <button onClick={toggleStart}
+          className='playButton'>
+          <i className={`fa-solid fa-${iconClass}`}></i>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
