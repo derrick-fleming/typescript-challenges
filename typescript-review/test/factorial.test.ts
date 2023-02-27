b56b4a3628b75f993ee81ffba8e4b9b71d9e4d20
Derrick Fleming <derrick.fleming7@gmail.com>
2023-02-27 23:00:31 +0000

    Completed javascript.info recursive challenge with factorials. Reviewed test creation with mocha and chai.

---
+++ typescript-review/test/factorial.test.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import factorial from '../factorial-practice';
+
+describe('recursiveSumTo function', () => {
+  beforeEach(function () {
+    expect(factorial).to.be.a('function');
+  })
+  it('returns the correct factorial of 4', () => {
+    const output = factorial(4);
+    expect(output).to.equal(24);
+  })
+  it('returns the correct factorial of 5', () => {
+    const output = factorial(5);
+    expect(output).to.equal(120);
+  })
+  it('returns the correct factorial of 7', () => {
+    const output = factorial(7);
+    expect(output).to.equal(5040);
+  })
+  it('returns the correct factorial of 0', () => {
+    const output = factorial(0);
+    expect(output).to.equal(1);
+  })
+})
